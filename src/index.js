016bd6a18ab364ac98fadb6e712f19667b94fae3
Odin Zeus <futapas@yahoo.com>
2023-08-27 12:43:45 +0300

    STEP 5 IS SET,NEXT COMES TO WRITE THE FUNCTION THAT LOADS THE ELEMENTS TO THE DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { functionOne } from './page_load';
 
-console.log('it runs i guess...')
+functionOne(); // this should work as expected!
+
+//Since we’re all set up to write our code in multiple files,
+// let’s write this initial page-load function inside of its own module
+// and then import and call it inside of index.js.
 
---
+++ src/page_load.js
@@ -0,0 +1,8 @@
+const functionOne = () => console.log('FUNCTION ONE!');
+
+export { functionOne };
+// leave only the <html>, <body>,
+// and <div id="content"> tags and instead create any new  element 
+// by using JavaScript only,
+// e.g. by appending each new element to div#content
+// once the page is first loaded
